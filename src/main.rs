c5bd7460860a136904eb3ac02d53c8596d640f08
Michał Hanusek <mhanusek@energotest.com.pl>
2020-02-20 13:34:00 +0100

    Init  commit

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+extern crate libmodbus_rs;
+
+use libmodbus_rs::prelude::*;
+use libmodbus_rs::{Modbus, ModbusClient, ModbusTCP, ModbusTCPPI, ModbusRTU,
+                   Exception, FunctionCode, Timeout, ErrorRecoveryMode};
+
+use std::{thread, time};
+
+const SERVER_ID: u8 = 1;
+
+fn main()
+{
+    println!("Hello, world!");
+    let mut modbus: Modbus;
+
+    modbus = Modbus::new_rtu("/dev/ttyUSB1", 115200, 'N', 8, 1).unwrap();
+    modbus.set_slave(SERVER_ID);
+
+    modbus.set_debug(true);
+    modbus.connect();
+
+    let old_response_timeout = modbus.get_response_timeout().expect("could not get response timeout");
+
+    match modbus.connect()
+    {
+        Err(err) => panic!("Connection failed: {}", err),
+        Ok(_) => {}
+    }
+
+    let new_response_timeout = modbus.get_response_timeout().expect("could not get response timeout");
+    println!("response_timeout : {:?}\n", new_response_timeout);
+
+    let mut tab_rp_registers = vec![0u16; 1024 as usize];
+
+    // Single register
+
+    let time_ms = time::Duration::from_millis(500);
+
+    let mut value = 0;
+    loop
+    {
+        value = value + 1;
+        let rc = modbus.write_register(0, value);
+        thread::sleep(time_ms);
+
+        let rc = modbus.read_registers(0, 1, &mut tab_rp_registers).unwrap();
+        thread::sleep(time_ms);
+    }
+}
